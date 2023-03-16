85dd05761713659a1fcd86fb5065f0c61f6f6fb0
foosuf <127121723+foosuf@users.noreply.github.com>
2023-03-16 15:32:50 +0300

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Redirect Example</title>
-</head>
-<body>
-	<button id="start-btn">Start</button>
-
-	<script>
-		document.getElementById("start-btn").addEventListener("click", function() {
+<script>
+	document.getElementById("start-btn").addEventListener("click", function() {
 			window.location.href = "game.html";
 		});
-	</script>
-</body>
-</html>
+</script>
+
