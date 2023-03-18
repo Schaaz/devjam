6290d778e406013ce894e95db0378292d8e63e00
Khanasad2026 <127110800+Khanasad2026@users.noreply.github.com>
2023-03-18 11:38:31 +0530

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -1,4 +1,54 @@
-document.getElementById("start-btn").addEventListener("click", function() {
-	window.location.href = "game.html";
-	});
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
+const optioncontainer = document.querySelector('.option-container')
+const flipbutton = document.querySelector('#flip-button')
 
+// Option choosing
+let angle = 0
+function flip(){
+const optionShips = Array.from(optionContainer.children)
+if(angle ===0){
+	angle =90
+}
+	else{
+		angle =0
+}
+optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(${angle}deg)')
+}
+flipbutton.addEventListener('click',flip)
+	
+//Creating Boards
+const width = 10
+
+function createBoard(color, user){
+  const gameBoardContainer = document.createElement('div')
+  gameBoardContainer.classList.add('game-board')
+  gameBoardContainer.style.backgroundColor = 'yellow'
+  gameBoardContainer.id = user
+  
+  for(let i=0; i<width * width; i++){
+	  const block = document.createElement('div')
+	  block.classlist.add('block')
+	  block.id = i
+	  gameBoardContainer.append(block)
+  }
+	
+  gamesBoardContainer.append(gameBoardContainer)
+}
+createBoard('blue', 'player')
+createBoard('Yellow', 'player')
+  
+// creating ships
+class Ship{
+	constructor(name, length){
+		this.name = name
+		this.length =length
+	}
+}
+
+const destroyer = new Ship('destroyer', 2)
+const submarine =new Ship('submarine', 3)
+const cruiser =new Ship('cruiser',3)
+const battleship =new Ship('battleship',4)
+const carrier=new Ship('carrier',5)
+
+	
