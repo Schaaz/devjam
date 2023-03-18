b405e908626acadf838d5721a708728a86172a05
Khanasad2026 <127110800+Khanasad2026@users.noreply.github.com>
2023-03-18 17:28:57 +0530

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -7,12 +7,13 @@ let angle = 0
 function flip(){
 const optionShips = Array.from(optionContainer.children)
 if(angle === 0){
+	optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(90deg)')
 	angle = 90
 }
 	else{
-		angle =0
+		optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(0deg)')
+		angle = 0
 }
-optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(${angle}deg)')
 }
 flipButton.addEventListener('click',flip)
 	
